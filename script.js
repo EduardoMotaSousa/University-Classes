1f5c976fc8feab0de20b1fecf0d85af462c11058
Eduardo Mota <eduardomotasousa2005@gmail.com>
2026-05-02 16:54:00 -0300

    ADD: HTML & CSS

---
+++ script.js
@@ -0,0 +1,52 @@
+const usuario = "EduardoMotaSousa";
+const repo = "University-Classes";
+
+let historico = [];
+
+async function carregarConteudo(pasta = "") {
+  const url = `https://api.github.com/repos/${usuario}/${repo}/contents/${pasta}`;
+  
+  const res = await fetch(url);
+  const dados = await res.json();
+
+  const container = document.getElementById("pastas");
+  container.innerHTML = "";
+
+  const ignorar = [".git", ".vscode", "node_modules", "README.md", "output"];
+
+  dados.forEach(item => {
+    if (ignorar.includes(item.name)) return;
+
+    const div = document.createElement("div");
+    div.className = "card";
+
+    if (item.type === "dir") {
+      div.innerHTML = `📁<br>${item.name}`;
+      div.onclick = () => {
+        historico.push(pasta);
+        carregarConteudo(item.path);
+      };
+    } else {
+      div.innerHTML = `📄<br>${item.name}`;
+      div.onclick = () => window.open(item.download_url, "_blank");
+    }
+
+    container.appendChild(div);
+  });
+
+  document.getElementById("caminho").textContent = pasta || "home";
+
+  document.getElementById("voltar").style.display =
+    historico.length ? "inline-block" : "none";
+}
+
+function voltar() {
+  const anterior = historico.pop();
+  carregarConteudo(anterior || "");
+}
+
+function irGithub() {
+  window.open("https://github.com/EduardoMotaSousa");
+}
+
+carregarConteudo();
