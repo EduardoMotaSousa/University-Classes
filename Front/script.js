b3aafe1ab2edd36be848428462cba2a049c7cc66
Eduardo Mota <eduardomotasousa2005@gmail.com>
2026-05-03 16:22:14 -0300

    Add: botão linkedin e botões de "+"

--- Front/script.js
+++ Front/script.js
@@ -2,8 +2,11 @@ const usuario = "EduardoMotaSousa";
 const repo = "University-Classes";
 
 let historico = [];
+let pastaAtual = "";
 
 async function carregarConteudo(pasta = "") {
+  pastaAtual = pasta;
+
   const url = `https://api.github.com/repos/${usuario}/${repo}/contents/${pasta}`;
   
   const res = await fetch(url);
@@ -38,6 +41,17 @@ async function carregarConteudo(pasta = "") {
     container.appendChild(div);
   });
 
+  // 🔥 BOTÃO "+"
+  const add = document.createElement("div");
+  add.className = "card-add";
+  add.innerHTML = "+";
+
+  add.onclick = () => {
+    window.open(`https://github.com/${usuario}/${repo}/tree/main/${pastaAtual}`, "_blank");
+  };
+
+  container.appendChild(add);
+
   document.getElementById("voltar").style.display =
     historico.length ? "inline-block" : "none";
 }
@@ -51,9 +65,13 @@ function irGithub() {
   window.open("https://github.com/EduardoMotaSousa");
 }
 
+function irLinkedin() {
+  window.open("https://www.linkedin.com/in/eduardomotaads/");
+}
+
 /* README */
 async function carregarReadme() {
-  const url = "https://raw.githubusercontent.com/EduardoMotaSousa/University-Classes/main/README.md";
+  const url = `https://raw.githubusercontent.com/${usuario}/${repo}/main/README.md`;
   
   const res = await fetch(url);
   const markdown = await res.text();
