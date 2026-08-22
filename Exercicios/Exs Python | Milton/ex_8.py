af026fb0267e581224be2e255389c798bbbca643
Eduardo Mota <134015632+EduardoMotaSousa@users.noreply.github.com>
2026-08-22 00:36:01 +0000

    Correção queda

--- Exercicios/Exs Python | Milton/ex_8.py
+++ Exercicios/Exs Python | Milton/ex_8.py
@@ -1,5 +1,6 @@
 '''Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
 Calcule e mostre o total do seu salário no referido mês.'''
+import math
 
-for i in range(500):
-    print(i ** i)
+for i in range(50000):
+    print((i ** math.pi)*6.5986595)
